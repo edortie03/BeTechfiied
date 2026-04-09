b85200da7214f0016ba272e61ce1eeba9c543e8f
Edward Denis <edorward.21@gmail.com>
2026-04-09 22:41:33 +0300

    First commit

---
+++ intro.js
@@ -0,0 +1,7 @@
+const details = {
+    names: "Edward Denis",
+    Age: 23, 
+    programming_language: "Javascript and Python" 
+}
+
+console.log("My name is:", details.names, "I am ",details.Age, "and I love",details.programming_language, "programming languages")
